cdb64f031d4fe7dc9d1218fcbbea4b80f68c4905
TomBombadiI <78550692+TomBombadiI@users.noreply.github.com>
2024-12-19 20:56:31 +0700

    add banner section

---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+const burgerBtns = document.querySelectorAll('.burger-open');
+const navMenu = document.querySelector('.nav-menu');
+
+burgerBtns.forEach(burgerBtn => {
+    burgerBtn.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
+});
+
+document.addEventListener('click', (e) => {
+    if (!e.target.closest('.nav-menu') && !e.target.closest('.burger-open')) {
+        navMenu.classList.remove('active');
+    }
+})
+
+const selects = document.querySelectorAll('.select');
+
+selects.forEach(select => {
+    const selectList = select.querySelector('.select-list');
+    const activeOption = select.querySelector('.option');
+    const options = selectList.querySelectorAll('.option');
+    const arrowDropdown = activeOption.querySelector('.arrow-dropdown');
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            const tmp = activeOption.cloneNode(true);
+            tmp.querySelector('.arrow-dropdown').remove();
+
+            activeOption.innerHTML = option.innerHTML;
+            activeOption.classList = option.classList;
+            activeOption.insertAdjacentElement('beforeend', arrowDropdown);
+            option.innerHTML = tmp.innerHTML;
+            option.classList = tmp.classList;
+        });
+    });
+
+    select.addEventListener('click', () => {
+        if (selectList.style.height === '0px' || !selectList.style.height) {
+            selectList.style.height = selectList.scrollHeight + 15 + 'px';
+            selectList.style.zIndex = '20';
+            select.style.overflow = 'visible';
+            select.querySelector('.option').style.zIndex = '21';
+            selectList.classList.remove('active');
+        } else {
+            selectList.classList.add('active');
+            selectList.style.height = '0';
+            selectList.style.zIndex = '5';
+            setTimeout(() => {
+                select.style.overflow = 'hidden';
+                select.querySelector('.option').style.zIndex = '6';
+            }, 300);
+        }
+    });
+
+});
+
+document.addEventListener('click', (event) => {
+    if (!event.target.closest('.select')) {
+        selects.forEach(select => {
+            const selectList = select.querySelector('.select-list');
+
+            selectList.style.height = '0';
+            selectList.style.zIndex = '5';
+        })
+    } else {
+        selects.forEach(select => {
+            if (select != event.target.closest('.select')) {
+                const selectList = select.querySelector('.select-list');
+
+                selectList.style.height = '0';
+                selectList.style.zIndex = '5';
+            }
+        })
+    }
+});
